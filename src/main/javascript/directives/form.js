05312e13a33396afe75ad4e276ca4b10b113be9c
tscheinecker <thomas.scheinecker@catalysts.cc>
2014-06-23 11:36:33 +0200

    extend the form directive to warn the user if the data is dirty

---
+++ src/main/javascript/directives/form.js
@@ -0,0 +1,46 @@
+'use strict';
+angular.module('cat')
+    .directive('form', ['$timeout', function ($timeout) {
+        return {
+            restrict: 'E',
+            scope: true,
+            require: 'form',
+            link: function (scope, element, attrs, formCtrl) {
+                var warningMessage = attrs.eocsWarnOnNavIfDirty || 'You have unsaved changes. Leave the page?';
+
+                // TODO - remove this ugly hack if ui-select2 fixes this problem...
+                $timeout(function () {
+                    formCtrl.$setPristine(true);
+                }, 50);
+
+                scope.$on('formReset', function () {
+                    formCtrl.$setPristine(true);
+                });
+
+                scope.$on('formDirty', function () {
+                    formCtrl.$setDirty(true);
+                });
+
+                // handle angular route change
+                scope.$on('$locationChangeStart', function (event) {
+                    if (formCtrl.$dirty) {
+                        if (!window.confirm(warningMessage)) {
+                            event.preventDefault();
+                        }
+                    }
+                });
+
+                // handle browser window/tab close
+                $(window).on('beforeunload', function (event) {
+                    if (formCtrl.$dirty) {
+                        return warningMessage;
+                    }
+                });
+
+                // clean up beforeunload handler when scope is destroyed
+                scope.$on('$destroy', function () {
+                    $(window).unbind('beforeunload');
+                });
+            }
+        };
+    }]);
