cd64c47452b5d40491137214a655b9984240b7b3
tscheinecker <thomas.scheinecker@catalysts.cc>
2014-06-23 11:36:33 +0200

    extend cat-select to support sorting and array data

--- src/main/javascript/directives/cat-select.js
+++ src/main/javascript/directives/cat-select.js
@@ -2,14 +2,13 @@
 
 angular.module('cat')
     .directive('catSelect', function ($log, $api) {
-        var fetchElements = function (endpoint) {
+        var fetchElements = function (endpoint, sort) {
             return function (queryParams) {
                 var searchRequest = new window.cat.SearchRequest(queryParams.data);
-                searchRequest.sort({ property: 'name', isDesc: false });
+                searchRequest.sort(sort || { property: 'name', isDesc: false });
                 return endpoint.list(searchRequest).then(queryParams.success);
             };
         };
-
 
         return {
             restrict: 'EA',
@@ -23,13 +22,34 @@ angular.module('cat')
                 element.addClass('form-control');
             },
             controller: function ($scope) {
-                var api = $api[$scope.options.endpoint];
-                if (!api) {
-                    $log.error('No api endpoint "' + $scope.options.endpoint + '" defined');
-                    $scope.elements = [];
-                    return;
+                var transport,
+                    quietMillis,
+                    searchRequestFunc = $scope.options.search || function (term, page) {
+                        return { 'search.name': term };
+                    },
+                    filterFunc = $scope.options.filter || function (term) {
+                        return true;
+                    };
+                if (Object.prototype.toString.call($scope.options.endpoint) === '[object Array]') {
+                    transport = function (queryParams) {
+                        return queryParams.success({
+                            elements: $scope.options.endpoint
+                        });
+                    };
+                    quietMillis = 0;
+                } else {
+                    var api = $api[$scope.options.endpoint];
+                    if (!api) {
+                        $log.error('No api endpoint "' + $scope.options.endpoint + '" defined');
+                        $scope.elements = [];
+                        return;
+                    }
+                    transport = fetchElements(api, $scope.options.sort);
+                    quietMillis = 500;
                 }
+
                 $scope.selectOptions = _.assign({
+                    placeholder: ' ', // space in default placeholder is required, otherwise allowClear property does not work
                     minimumInputLength: 0,
                     adaptDropdownCssClass: function (cssClass) {
                         if (_.contains(['ng-valid', 'ng-invalid', 'ng-pristine', 'ng-dirty'], cssClass)) {
@@ -38,13 +58,11 @@ angular.module('cat')
                         return null;
                     },
                     ajax: {
-                        data: function (term, page) {
-                            return { 'search.name': term };
-                        },
-                        quietMillis: 500,
-                        transport: fetchElements(api),
+                        data: searchRequestFunc,
+                        quietMillis: quietMillis,
+                        transport: transport,
                         results: function (data, page) {
-                            return {results: data.elements};
+                            return {results: _.filter(data.elements, filterFunc)};
                         }
                     },
                     formatResult: function (element) {
