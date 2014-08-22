eb4b793efb57507628755e62ebe7a56c0972cf29
Thomas Scheinecker <thomas.scheinecker@catalysts.cc>
2014-08-22 11:11:52 +0200

    introduced catSelectConfigService which holds arbitrary config objects

---
+++ src/main/javascript/service/cat-select-config-service.js
@@ -0,0 +1,44 @@
+/**
+ * Created by tscheinecker on 05.08.2014.
+ */
+
+'use strict';
+
+function assignDeep(target, source) {
+    return _.assign(target, source, function (targetProperty, sourceProperty) {
+        if (_.isObject(targetProperty) && _.isObject(sourceProperty)) {
+            return assignDeep(targetProperty, sourceProperty);
+        }
+
+        return sourceProperty;
+    });
+}
+
+function CatSelectConfigService(configs) {
+    var _configs = configs;
+
+    this.getConfig = function (name, options) {
+        var config = configs[name];
+
+        if (_.isUndefined(config) && _.isUndefined(options)) {
+            return undefined;
+        }
+
+        return assignDeep(_.clone(config) || {}, options);
+    };
+}
+
+function CatSelectConfigServiceProvider() {
+    var configs = {};
+
+    this.config = function (name, config) {
+        configs[name] = config;
+    };
+
+    this.$get = function () {
+        return new CatSelectConfigService(configs);
+    };
+}
+
+
+angular.module('cat.service').provider('catSelectConfigService', CatSelectConfigServiceProvider);
