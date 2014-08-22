89e97f76cc97ef52744a8da8307c8ed833f49189
Thomas Scheinecker <thomas.scheinecker@catalysts.cc>
2014-08-22 11:31:14 +0200

    add some documentation to Facet.js

--- src/main/javascript/base/Facet.js
+++ src/main/javascript/base/Facet.js
@@ -2,6 +2,15 @@
 
 window.cat = window.cat || {};
 
+/**
+ * A 'FacetTerm' model used in conjunction with the cat-paginated directive where it represents a value of a group or
+ * property which can be used to filter the shown list.
+ * It consist of an id, a name and a count
+ *
+ * @param {Object} [data={}] the data used instantiate the object with. Usually this is the object representation
+ * returned from the server.
+ * @constructor
+ */
 window.cat.FacetTerm = function (data) {
     if (data === undefined) data = {};
 
@@ -10,6 +19,15 @@ window.cat.FacetTerm = function (data) {
     this.count = data.count;
 };
 
+/**
+ * A 'Facet' model which is used in conjunction with the cat-paginated directive where it represents a group or
+ * which property which can be used to filter the shown list.
+ * It has a name and an array of FacetTerms
+ *
+ * @param {Object} [data={}] the data used instantiate the object with. Usually this is the object representation
+ * returned from the server.
+ * @constructor
+ */
 window.cat.Facet = function (data) {
     if (data === undefined) data = {};
 
