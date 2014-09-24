c53abe0c1eae579d747d4301ecce5352a521b3fb
Thomas Scheinecker <thomas.scheinecker@catalysts.cc>
2014-09-24 13:36:51 +0200

    introduced cat-naming-util.js with a method for pluralizing strings (y -> ies, s -> ses, * -> *s)

---
+++ src/main/javascript/util/cat-naming-util.js
@@ -0,0 +1,23 @@
+/**
+ * Created by tscheinecker on 26.08.2014.
+ */
+'use strict';
+
+window.cat.util = window.cat.util || {};
+
+window.cat.util.pluralize = function (string) {
+    if (_.isUndefined(string) || string.length === 0) {
+        return '';
+    }
+    var lastChar = string[string.length - 1];
+
+    switch (lastChar) {
+        case 'y':
+            return string.substring(0, string.length - 1) + 'ies';
+        case 's':
+            return string + 'es';
+        default :
+            return string + 's';
+    }
+
+};
