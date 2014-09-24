15b96c31971de8fcc508be0738a82e77ed0bed56
Thomas Scheinecker <thomas.scheinecker@catalysts.cc>
2014-09-24 13:52:47 +0200

    improved catRouteService and catViewService to use the 'pluralize' util function for url generation

--- src/main/javascript/service/cat-route-service.js
+++ src/main/javascript/service/cat-route-service.js
@@ -4,14 +4,31 @@
 
 'use strict';
 
+/**
+ * @ngdoc service
+ * @description
+ * This service provider delegates to the $routeProvider and actually creates 2 separate routes after applying various
+ * conventions / defaults
+ *
+ * @param $routeProvider
+ * @constructor
+ */
 function CatRouteServiceProvider($routeProvider) {
     var viewNames = [];
 
+    /**
+     * This function creates route urls via convention from the given parameters and passes them (together with the
+     * configuration) to the $routeProvider. The actual route configuration is received by passing the given one
+     * to #window.cat.util.route.list and #window.cat.util.route.detail
+     * @param {string} baseUrl the base url which will be prepended to all routes
+     * @param {string} name the name for which the routes will be created
+     * @param {Object} [config] the config object which wraps the configurations for the list and detail route
+     */
     this.listAndDetailRoute = function (baseUrl, name, config) {
         viewNames.push(name);
 
 
-        var listUrl = baseUrl + '/' + name.toLowerCase() + 's';
+        var listUrl = baseUrl + '/' + window.cat.util.pluralize(name.toLowerCase());
 
         if (!!config && config.url) {
             listUrl = baseUrl + '/' + config.url || listUrl;
@@ -28,7 +45,11 @@ function CatRouteServiceProvider($routeProvider) {
             .when(listUrl + '/:id', window.cat.util.route.detail(_.assign({}, nameConf, detailsConfig)));
     };
 
-
+    /**
+     * This service simply exposes the created view and endpoint names, as the provider basically only delegates
+     * to the $routeProvider
+     * @return {Array} the registered view names
+     */
     this.$get = function () {
         return viewNames;
     };
--- src/main/javascript/service/cat-view-service.js
+++ src/main/javascript/service/cat-view-service.js
@@ -4,13 +4,31 @@
 
 'use strict';
 
+/**
+ * @ngdoc service
+ * @description
+ * This service provider can be used to initialize an api endpoint and the according detail and list routes by simply
+ * providing a name and a config object.
+ *
+ * @param {CatRouteServiceProvider} catRouteServiceProvider
+ * @param {CatApiServiceProvider} catApiServiceProvider
+ * @constructor
+ */
 function CatViewServiceProvider(catRouteServiceProvider, catApiServiceProvider) {
     var viewNames = [];
     var endpointNames = [];
 
+    /**
+     * This function registers a new api endpoint with catApiServiceProvider and a list and detail route with
+     * catRouteServiceProvider
+     * @param {string} baseUrl the base url which will be prepended to all generated route pats
+     * @param {string} name the name used as entry point to all routes and endpoint creations (camel cased)
+     * @param {object} [config] the config object which can in turn hold objects used for configuration of the endpoint,
+     * detail route or list route
+     */
     this.listAndDetailView = function (baseUrl, name, config) {
         var endpointName = name.toLowerCase();
-        var url = endpointName + 's';
+        var url = window.cat.util.pluralize(endpointName);
 
         if (!!config) {
             url = config.url || url;
@@ -35,7 +53,11 @@ function CatViewServiceProvider(catRouteServiceProvider, catApiServiceProvider) 
         catRouteServiceProvider.listAndDetailRoute(baseUrl, name, config);
     };
 
-
+    /**
+     * This service simply exposes the created view and endpoint names, as the provider basically only delegates
+     * to other service providers
+     * @return {{views: Array, endpoints: Array}}
+     */
     this.$get = function () {
         return {
             views: viewNames,
