0296086278fae1f61c9f9598ba17c95a6dd2e51a
Thomas Scheinecker <thomas.scheinecker@catalysts.cc>
2015-02-25 15:16:52 +0100

    push api to gh-pages

---
+++ js/docs-setup.js
@@ -0,0 +1,389 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "cat.controller.base.detail:CatBaseDetailController",
+      "shortName": "CatBaseDetailController",
+      "type": "controller",
+      "moduleName": "cat.controller.base",
+      "shortDescription": "The CatBaseDetailController takes care of providing several common properties and functions to the scope",
+      "keywords": "$controller $fielderrors $globalmessages $location $log $scope $state $stateparams $window actual add additional additionalviewtemplate api array base breadcrumbs canceledit care cat catbasedetailcontroller catbreadcrumbsservice changes common config controller copy create current data delete detail doctodo edit editdetail editing edittemplate endpoint errors exist exists function functions holds include instantiates main mainviewtemplate map mode model object parameter parents properties providing remove resolve returned save scope server shares stack switch takes template title ui uistack update url urls validation view"
+    },
+    {
+      "section": "api",
+      "id": "cat.controller.base.list:CatBaseListController",
+      "shortName": "CatBaseListController",
+      "type": "controller",
+      "moduleName": "cat.controller.base",
+      "shortDescription": "The CatBaseListController takes care of providing several common properties to the scope",
+      "keywords": "$controller $log $scope addition api base button care cat cat-base-list cat-paginated catbaselistcontroller catbreadcrumbsservice catlistdataloadingservice common config controller data directive holds html include instantiate instantiates list listdata log model object parameter passed properties providing scope search searchprops shares takes template title tpl url view"
+    },
+    {
+      "section": "api",
+      "id": "cat.controller.base.tabs:CatBaseTabsController",
+      "shortName": "CatBaseTabsController",
+      "type": "controller",
+      "moduleName": "cat.controller.base",
+      "keywords": "api base cat controller tabs"
+    },
+    {
+      "section": "api",
+      "id": "cat.directives.autofocus:catAutofocus",
+      "shortName": "catAutofocus",
+      "type": "directive",
+      "moduleName": "cat.directives",
+      "keywords": "api autofocus cat directive directives"
+    },
+    {
+      "section": "api",
+      "id": "cat.directives.checkbox:catCheckbox",
+      "shortName": "catCheckbox",
+      "type": "directive",
+      "moduleName": "cat.directives",
+      "keywords": "api cat checkbox directive directives"
+    },
+    {
+      "section": "api",
+      "id": "cat.directives.confirmClick:catConfirmClick",
+      "shortName": "catConfirmClick",
+      "type": "directive",
+      "moduleName": "cat.directives",
+      "keywords": "api cat confirmclick directive directives"
+    },
+    {
+      "section": "api",
+      "id": "cat.directives.facets:catFacets",
+      "shortName": "catFacets",
+      "type": "directive",
+      "moduleName": "cat.directives",
+      "keywords": "api cat directive directives facets"
+    },
+    {
+      "section": "api",
+      "id": "cat.directives.fieldError:catFieldErrors",
+      "shortName": "catFieldErrors",
+      "type": "directive",
+      "moduleName": "cat.directives",
+      "keywords": "api cat directive directives fielderror"
+    },
+    {
+      "section": "api",
+      "id": "cat.directives.form:form",
+      "shortName": "form",
+      "type": "directive",
+      "moduleName": "cat.directives",
+      "keywords": "api cat directive directives form"
+    },
+    {
+      "section": "api",
+      "id": "cat.directives.i18n:catI18n",
+      "shortName": "catI18n",
+      "type": "directive",
+      "moduleName": "cat.directives",
+      "keywords": "api cat directive directives i18n"
+    },
+    {
+      "section": "api",
+      "id": "cat.directives.inputs:catInputGroup",
+      "shortName": "catInputGroup",
+      "type": "directive",
+      "moduleName": "cat.directives",
+      "keywords": "api cat directive directives inputs"
+    },
+    {
+      "section": "api",
+      "id": "cat.directives.inputs:input",
+      "shortName": "input",
+      "type": "directive",
+      "moduleName": "cat.directives",
+      "keywords": "api cat directive directives inputs"
+    },
+    {
+      "section": "api",
+      "id": "cat.directives.loadMore:catLoadMore",
+      "shortName": "catLoadMore",
+      "type": "directive",
+      "moduleName": "cat.directives",
+      "keywords": "api cat directive directives loadmore"
+    },
+    {
+      "section": "api",
+      "id": "cat.directives.menu:catLoadMore",
+      "shortName": "catLoadMore",
+      "type": "directive",
+      "moduleName": "cat.directives",
+      "keywords": "api cat directive directives menu"
+    },
+    {
+      "section": "api",
+      "id": "cat.directives.numbersOnly:numbersOnly",
+      "shortName": "numbersOnly",
+      "type": "directive",
+      "moduleName": "cat.directives",
+      "keywords": "api cat directive directives numbersonly"
+    },
+    {
+      "section": "api",
+      "id": "cat.directives.paginated:catPaginated",
+      "shortName": "catPaginated",
+      "type": "directive",
+      "moduleName": "cat.directives",
+      "keywords": "api cat directive directives paginated"
+    },
+    {
+      "section": "api",
+      "id": "cat.directives.select:catSelect",
+      "shortName": "catSelect",
+      "type": "directive",
+      "moduleName": "cat.directives",
+      "shortDescription": "The &#39;cat-select&#39; directive is a wrapper around the &#39;ui-select2&#39; directive which adds support for using an api",
+      "keywords": "adds api approaches array attribute call case cat cat-select catapiservice catselectconfigservice config configuration controller correct default defines direction directive directives directly doc ea easily endpoint exist format function link list named object options overridden passed priority properties property provided replace represents restrict retrieved retrieving returns reuse scope select sort source support supported supports template todo transport treated ui-select2 values ways wrapper"
+    },
+    {
+      "section": "api",
+      "id": "cat.directives.sortable:catSortable",
+      "shortName": "catSortable",
+      "type": "directive",
+      "moduleName": "cat.directives",
+      "keywords": "api cat directive directives sortable"
+    },
+    {
+      "section": "api",
+      "id": "cat.filters.replaceText:replaceText",
+      "shortName": "replaceText",
+      "type": "filter",
+      "moduleName": "cat.filters",
+      "shortDescription": "Replaces text passages with other text, based on regular expressions",
+      "keywords": "api based cat expression expressions filter filters options original passages pattern regular replacement replaces replacetext text"
+    },
+    {
+      "section": "api",
+      "id": "cat.service.api:$api",
+      "shortName": "$api",
+      "type": "service",
+      "moduleName": "cat.service",
+      "shortDescription": "deprectaed use &#39;$api&#39;",
+      "keywords": "$api api cat catapiserviceprovider configuration configurations create deprectaed endpoint exposes method named retrieve service single"
+    },
+    {
+      "section": "api",
+      "id": "cat.service.api:$api",
+      "shortName": "$api",
+      "type": "service",
+      "moduleName": "cat.service",
+      "shortDescription": "The CatApiServiceProvider exposes a single configuration method &#39;endpoint&#39; which can be used to create or retrieve",
+      "keywords": "api cat catapiserviceprovider configuration configurations create endpoint exposes method named retrieve service single"
+    },
+    {
+      "section": "api",
+      "id": "cat.service.breadcrumbs:catBreadcrumbs",
+      "shortName": "catBreadcrumbs",
+      "type": "overview",
+      "moduleName": "cat.service",
+      "keywords": "api breadcrumbs cat overview service"
+    },
+    {
+      "section": "api",
+      "id": "cat.service.breadcrumbs:catBreadcrumbService",
+      "shortName": "catBreadcrumbService",
+      "type": "service",
+      "moduleName": "cat.service",
+      "shortDescription": "This service is a simple wrapper around a list of Objects.",
+      "keywords": "api breadcrumb breadcrumbs cat convenience cumbersome handling list main manipulating methods objects purpose service simple wrapper"
+    },
+    {
+      "section": "api",
+      "id": "cat.service.httpIntercept:errorHttpInterceptor",
+      "shortName": "errorHttpInterceptor",
+      "type": "service",
+      "moduleName": "cat.service",
+      "keywords": "api cat httpintercept service"
+    },
+    {
+      "section": "api",
+      "id": "cat.service.i18n:CAT_I18N_DEFAULT_LOCALE",
+      "shortName": "CAT_I18N_DEFAULT_LOCALE",
+      "type": "overview",
+      "moduleName": "cat.service",
+      "shortDescription": "The default locale used for message translation",
+      "keywords": "api cat default i18n locale message overview service translation"
+    },
+    {
+      "section": "api",
+      "id": "cat.service.i18n:catI18nLocaleService",
+      "shortName": "catI18nLocaleService",
+      "type": "service",
+      "moduleName": "cat.service",
+      "keywords": "api cat i18n service"
+    },
+    {
+      "section": "api",
+      "id": "cat.service.i18n:catI18nMessageParameterResolver",
+      "shortName": "catI18nMessageParameterResolver",
+      "type": "service",
+      "moduleName": "cat.service",
+      "shortDescription": "A function which accepts a message and parameters and returns the resolved message",
+      "keywords": "accepts api cat function i18n message parameters resolved returns service"
+    },
+    {
+      "section": "api",
+      "id": "cat.service.i18n:catI18nMessageSourceService",
+      "shortName": "catI18nMessageSourceService",
+      "type": "service",
+      "moduleName": "cat.service",
+      "shortDescription": "A service to retrieve message templates for a given key and locale",
+      "keywords": "$cat_i18n_default_locale $q api cat doctodo i18n key locale message retrieve service templates"
+    },
+    {
+      "section": "api",
+      "id": "cat.service.i18n:catI18nService",
+      "shortName": "catI18nService",
+      "type": "service",
+      "moduleName": "cat.service",
+      "shortDescription": "A service to translate message keys to messages of specific locales",
+      "keywords": "$q api cat cati18nmessageparameterresolver cati18nmessagesourceservice doctodo i18n locales message messages service specific translate"
+    },
+    {
+      "section": "api",
+      "id": "cat.service.listDataLoading:catListDataLoadingService",
+      "shortName": "catListDataLoadingService",
+      "type": "service",
+      "moduleName": "cat.service",
+      "keywords": "api cat listdataloading service"
+    },
+    {
+      "section": "api",
+      "id": "cat.service.loading:loadingService",
+      "shortName": "loadingService",
+      "type": "service",
+      "moduleName": "cat.service",
+      "keywords": "api cat loading service"
+    },
+    {
+      "section": "api",
+      "id": "cat.service.menu:$mainMenu",
+      "shortName": "$mainMenu",
+      "type": "service",
+      "moduleName": "cat.service",
+      "keywords": "api cat menu service"
+    },
+    {
+      "section": "api",
+      "id": "cat.service.message:$globalMessages",
+      "shortName": "$globalMessages",
+      "type": "service",
+      "moduleName": "cat.service",
+      "keywords": "api cat message service"
+    },
+    {
+      "section": "api",
+      "id": "cat.service.route:catRouteService",
+      "shortName": "catRouteService",
+      "type": "service",
+      "moduleName": "cat.service",
+      "shortDescription": "This service provider delegates to the $stateProvider and actually creates 2 separate routes after applying various",
+      "keywords": "$stateprovider api applying cat conventions creates defaults delegates provider route routes separate service"
+    },
+    {
+      "section": "api",
+      "id": "cat.service.selectConfig:catSelectConfigService",
+      "shortName": "catSelectConfigService",
+      "type": "service",
+      "moduleName": "cat.service",
+      "keywords": "api cat selectconfig service"
+    },
+    {
+      "section": "api",
+      "id": "cat.service.view:catViewService",
+      "shortName": "catViewService",
+      "type": "service",
+      "moduleName": "cat.service",
+      "shortDescription": "This service provider can be used to initialize an api endpoint and the according detail and list routes by simply",
+      "keywords": "api cat catapiserviceprovider catrouteserviceprovider config detail doctodo endpoint initialize list object provider providing routes service simply view"
+    },
+    {
+      "section": "api",
+      "id": "catConversionService",
+      "shortName": "catConversionService",
+      "type": "service",
+      "moduleName": "catConversionService",
+      "shortDescription": "This service handles the transformation between server and client side data.",
+      "keywords": "api catconversionservice client data handles server service side transformation"
+    },
+    {
+      "section": "api",
+      "id": "EndpointConfig",
+      "shortName": "EndpointConfig",
+      "type": "service",
+      "moduleName": "EndpointConfig",
+      "shortDescription": "An &#39;EndpointConfig&#39; basically is a wrapper around the configuration for an api endpoint during the configuration",
+      "keywords": "actual api appropriate arbitrary attribute automatically basically call catapiendpoints child children config configuration configurations create deps endpoint endpointconfig endpoints exposes function helper instantiate manually map method model phase receive service url works wrapper wraps"
+    },
+    {
+      "section": "api",
+      "id": "Facet",
+      "shortName": "Facet",
+      "type": "overview",
+      "moduleName": "Facet",
+      "shortDescription": "A &#39;Facet&#39; model which is used in conjunction with the cat-paginated directive where it represents a group or",
+      "keywords": "api array cat-paginated conjunction data directive facet facetterms filter group instantiate list model object overview property representation represents returned server"
+    },
+    {
+      "section": "api",
+      "id": "FacetTerm",
+      "shortName": "FacetTerm",
+      "type": "overview",
+      "moduleName": "FacetTerm",
+      "shortDescription": "A &#39;FacetTerm&#39; model used in conjunction with the cat-paginated directive where it represents a value of a group or",
+      "keywords": "api cat-paginated conjunction consist count data directive facetterm filter group instantiate list model object overview property representation represents returned server"
+    },
+    {
+      "section": "api",
+      "id": "SearchRequest",
+      "shortName": "SearchRequest",
+      "type": "overview",
+      "moduleName": "SearchRequest",
+      "shortDescription": "A &#39;SearchRequest&#39; model used by the catApiService to provide the backend with certain filter, order, page and size",
+      "keywords": "api backend catapiservice current filter initialize model object order overview parameters properties provide representing search searchrequest size url"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api",
+    "scripts": [
+      "js/angular.min.js",
+      "js/angular-animate.min.js",
+      "js/marked.js",
+      "js/prettify.js"
+    ],
+    "styles": [],
+    "title": "cat-angular",
+    "html5Mode": true,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {},
+    "titleLink": "/api",
+    "loadDefaults": {
+      "angular": true,
+      "angularAnimate": true,
+      "marked": true,
+      "prettify": true
+    }
+  },
+  "html5Mode": true,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js",
+    "angular-animate.min.js",
+    "marked.js",
+    "prettify.js"
+  ]
+};
