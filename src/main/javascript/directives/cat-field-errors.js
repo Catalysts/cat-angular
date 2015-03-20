6d82375f4055c8c832bf7fb7ab3117dc060e19c6
Thomas Scheinecker <thomas.scheinecker@catalysts.cc>
2015-03-20 07:17:30 +0100

    [FIX] add workaround for missing 'bindToController' in angular 1.2

--- src/main/javascript/directives/cat-field-errors.js
+++ src/main/javascript/directives/cat-field-errors.js
@@ -15,12 +15,20 @@ angular.module('cat.directives.fieldErrors')
             bindToController: true,
             controllerAs: 'catFieldErrors',
             controller: function CatFieldErrorsController($scope, catValidationService) {
+                var that = this;
+
+                if (angular.version.major === 1 && angular.version.minor === 2) {
+                    $scope.$watch('name', function(name) {
+                        that.name = name;
+                    });
+                }
+
                 this.hasErrors = function() {
-                    return catValidationService.hasFieldErrors($scope.name);
+                    return catValidationService.hasFieldErrors(that.name);
                 };
 
                 this.getErrors = function() {
-                    return catValidationService.getFieldErrors($scope.name);
+                    return catValidationService.getFieldErrors(that.name);
                 };
             },
             template: '<div class="label label-danger" ng-if="catFieldErrors.hasErrors()"><ul><li ng-repeat="error in catFieldErrors.getErrors()">{{error}}</li></ul></div>'
