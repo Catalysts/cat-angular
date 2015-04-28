3c06791092fe1558c492a0063e3d6260b934dee4
Thomas Scheinecker <thomas.scheinecker@catalysts.cc>
2015-04-28 15:36:10 +0200

    add new element visibility service and directive to allow applications to hide certain ui elements

---
+++ src/main/javascript/service/cat-element-visibility-service.js
@@ -0,0 +1,30 @@
+'use strict';
+
+
+/**
+ * @ngdoc service
+ * @name cat.service.elementVisibility:catElementVisibilityService
+ * @service
+ *
+ * @description
+ * This service provides a entry point for handling the visibility of ui elements.
+ * The basic implementation always returns true and is intended to be decorated accordingly within production environments.
+ *
+ * @constructor
+ */
+function CatElementVisibilityService() {
+
+    /**
+     * A helper function to determine wheter or not a ui element should be visible.
+     *
+     * @param {string} identifier an identifier upon which the the implementation can mofiy it's behaviour
+     * @param data the data representing the element for which the visibility check is performed
+     * @returns {boolean} <code>true</code> if the element should be rendered, <code>false</code> otherwise
+     */
+    this.isVisible = function (identifier, data) {
+        return true;
+    };
+}
+
+angular.module('cat.service.elementVisibility', [])
+    .service('catElementVisibilityService', [CatElementVisibilityService]);
