445e37392a03db9d7ea8b50e8f078746e351f78f
Thomas Pinetz <thomas.pinetz@catalysts.cc>
2015-07-08 09:39:39 +0200

    Added the localized custom delete message functionality with params

--- src/main/javascript/directives/cat-confirm-click.js
+++ src/main/javascript/directives/cat-confirm-click.js
@@ -3,18 +3,34 @@
 /**
  * @ngdoc directive
  * @name cat.directives.confirmClick:catConfirmClick
+ * 
+ * @param cat-i18n-params cat-i18n-params need to be parsed as JSON or it will not work
  */
 angular.module('cat.directives.confirmClick', [])
-    .directive('catConfirmClick', function CatConfirmClickDirective() {
+    .directive('catConfirmClick', function CatConfirmClickDirective(catI18nService) {
         return {
             restrict: 'A',
             link: function CatConfirmClickLink(scope, element, attr) {
                 var msg = attr.catConfirmClick || 'Are you sure?';
                 var clickAction = attr.catOnConfirm;
+                var params = undefined;
+                if (!!attr.catI18nParams) {
+                    params = JSON.parse(attr.catI18nParams);
+                }
                 element.bind('click', function (event) {
-                    if (window.confirm(msg)) {
-                        scope.$eval(clickAction);
+                    function dialog(dialogMessage) {
+                        if (window.confirm(dialogMessage)) {
+                            scope.$eval(clickAction);
+                        }
                     }
+
+                    catI18nService.translate(msg, params).then(
+                        function (message) {
+                            dialog(message);
+                        }, function (reason) {
+                            dialog(msg);
+                        }
+                    );
                 });
             }
         };
