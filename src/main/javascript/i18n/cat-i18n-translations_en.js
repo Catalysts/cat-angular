073b8dcb2e99a6c115879188321e54317c19251a
Andreas Gerstmayr <andreas.gerstmayr@catalysts.cc>
2015-08-04 18:35:46 +0200

    add missing translations

--- src/main/javascript/i18n/cat-i18n-translations_en.js
+++ src/main/javascript/i18n/cat-i18n-translations_en.js
@@ -9,5 +9,9 @@ window.cat.i18n.en = window.cat.i18n.en || {};
 _.assign(window.cat.i18n.en, {
     'cc.catalysts.cat-paginated.itemsFound': '{{count}} entries found. Entries {{firstResult}}-{{lastResult}}',
     'cc.catalysts.cat-paginated.noItemsFound': 'No entries found',
-    'cc.catalysts.general.new': 'New'
+    'cc.catalysts.general.new': 'New',
+    'cc.catalysts.general.edit': 'Edit',
+    'cc.catalysts.general.delete': 'Delete',
+    'cc.catalysts.general.save': 'Save',
+    'cc.catalysts.general.cancel': 'Cancel'
 });
