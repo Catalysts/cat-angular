9e0bd3743414ac6be4389d319b1ec644a36a1f6e
Thomas Scheinecker <thomas.scheinecker@catalysts.cc>
2015-12-17 13:59:10 +0100

    migrate cat-messages directive

---
+++ src/main/typescript/directives/cat-messages.ts
@@ -0,0 +1,55 @@
+interface CatMessagesDirectiveScope extends IScope {
+    contextId?:string;
+    type?:string;
+    hasMessages():boolean;
+    getMessages():string[];
+}
+
+class CatMessagesController {
+    constructor($scope:CatMessagesDirectiveScope,
+                catValidationService:ICatValidationService) {
+        if (!$scope.type) {
+            $scope.type = 'error';
+        }
+
+        $scope.hasMessages = () => {
+            return catValidationService.hasGlobalErrors($scope.contextId);
+        };
+
+        $scope.getMessages = () => {
+            return catValidationService.getGlobalErrors($scope.contextId);
+        };
+    }
+}
+function catMessagesDirectiveFactory():IDirective {
+    return {
+        restrict: 'A',
+        templateUrl: 'template/cat-messages.tpl.html',
+        scope: {
+            type: '=?'
+        },
+        require: '?^^catValidationGroup',
+        link: (scope:CatMessagesDirectiveScope,
+               elem:IAugmentedJQuery,
+               attr:IAttributes,
+               catValidationGroupCtrl:ICatValidationGroupController) => {
+            if (!!catValidationGroupCtrl) {
+                scope.contextId = catValidationGroupCtrl.getContextId();
+            }
+        },
+        controller: [CatMessagesController]
+    };
+}
+
+angular
+    .module('cat.directives.messages', [
+        'cat.config.messages',
+        'cat.service.validation'
+    ])
+    /**
+     * @ngdoc directive
+     * @name cat.directives.messages:catMessages
+     */
+    .directive('catMessages', [
+        catMessagesDirectiveFactory
+    ]);
